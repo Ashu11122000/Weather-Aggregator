f0281f858d592e15ecad5f107a13d7d756a2546f
Ashish Sharma <51910671+Ashu11122000@users.noreply.github.com>
2025-02-19 21:45:37 +0530

    Add files via upload

---
+++ weather-data-aggregator/weather-data-aggregator/src/main/java/com/weather/weather_data_aggregator/WeatherDataAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.weather.weather_data_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherDataAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherDataAggregatorApplication.class, args);
+	}
+
+}
---
+++ weather-data-aggregator/weather-data-aggregator/src/test/java/com/weather/weather_data_aggregator/WeatherDataAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.weather_data_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
